951f3c8d66286f739f2c68fc7d6d2f39d75b37c4
Douglas Silva <douglasitpro@gmail.com>
2026-03-03 18:31:29 -0300

    Adicionadno Views para Cadastrar_Pessoas |  Listar Pessoas

--- cadastro/views.py
+++ cadastro/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import PessoaForm
+from django.contrib import messages
+from .models import Pessoa
 
 
 
 def index(request):
     return render(request, 'cadastro/index.html')
+
+
+def cadastrar_pessoa(request):
+    if request.method == "POST":
+        form = PessoaForm(request.POST)
+        if form.is_valid():
+            pessoa = form.save()
+            messages.success(request, f'{pessoa.nome} cadastrado com sucesso!')
+            return redirect('listar_pessoas')
+    else:
+        form = PessoaForm()
+
+def listar_pessoas(request):
+    pessoas = Pessoa.objects.all()
+    return render(request, 'cadastro/listar.html', {'pessoas':'pessoas'})
+
