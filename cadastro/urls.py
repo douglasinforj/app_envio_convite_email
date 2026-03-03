b58444b584dd21d0247ad019fa12c67f7ceeaece
Douglas Silva <douglasitpro@gmail.com>
2026-03-03 20:58:20 -0300

    Criada rota detalhes com PK do cadastro

--- cadastro/urls.py
+++ cadastro/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('cadastrar/', views.cadastrar_pessoa, name='cadastrar'),
     path('listar/', views.listar_pessoas, name='listar_pessoas'),
+    path('pessoa/<int:pk>/', views.detalhes_pessoa, name='detalhes_pessoa'),
 
 ]
