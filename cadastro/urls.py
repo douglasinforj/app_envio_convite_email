8c0039f88976556b09cd1ad0400586bcec780519
Douglas Silva <douglasitpro@gmail.com>
2026-03-04 08:28:02 -0300

    Rota para enviar_convite

--- cadastro/urls.py
+++ cadastro/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('cadastrar/', views.cadastrar_pessoa, name='cadastrar'),
     path('listar/', views.listar_pessoas, name='listar_pessoas'),
     path('pessoa/<int:pk>/', views.detalhes_pessoa, name='detalhes_pessoa'),
+    path('pessoa/<int:pk>/convite', views.enviar_convite, name='enviar_convite'),
 
 ]
