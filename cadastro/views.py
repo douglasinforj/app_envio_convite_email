05d946762f19626252a07a937acdc8f6fb6cac65
Douglas Silva <douglasitpro@gmail.com>
2026-03-04 08:27:26 -0300

    Criada a views para enviar_convite

--- cadastro/views.py
+++ cadastro/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect, get_list_or_404
-from .forms import PessoaForm
+from .forms import PessoaForm, ConviteForm
 from django.contrib import messages
 from .models import Pessoa
+from django.template.loader import render_to_string
+from django.core.mail import EmailMultiAlternatives
+from django.conf import settings
 
 
 
@@ -30,4 +33,55 @@ def listar_pessoas(request):
 def detalhes_pessoa(request, pk):
     pessoa = get_list_or_404(Pessoa, pk=pk)
     return render(request, 'cadastro/detalhes.html', {'pessoa':pessoa})
+
+
+def enviar_convite(request, pk):
+    pessoa = get_list_or_404(Pessoa, pk=pk)
+
+    if request.method == 'POST':
+        form = ConviteForm(request.POST)
+        if form.is_valid():
+            assunto = form.cleaned_data['assunto']
+            mensagem = form.cleaned_data['mensagem']
+
+            try:
+                # Renderiza template html
+                html_content = render_to_string('emails/convite.html', {
+                    'pessoa': pessoa,
+                    'mensagem': mensagem
+                })
+
+                #cria e-mail
+                email = EmailMultiAlternatives(
+                    subject=assunto,
+                    body=mensagem, #em texto
+                    from_email=settings.EMAIL_HOST_USER,
+                    to=[pessoa.email]
+                )
+
+                #Anexar versao HTML
+                email.attach_alternative(html_content, "text/html")
+
+                #Enviar e-mail
+                email.send()
+
+                #Marcar convite como enviado
+                pessoa.enviar_convite()       #usa a função na model
+
+                messages.success(request, f'Convite enviado para {pessoa.nome}!')
+                return redirect('detalhes_pessoa', pk=pessoa.pk)
+            
+            except Exception as e:
+                messages.error(request, f'Erro ao enviar e-mail: {str(e)}')
+    else:
+        form = ConviteForm()
+    
+    return render(request, 'cadastro/enviar_convite.html', {
+        'form': form,
+        'pessoa': pessoa
+    })
+
+
+
+
 
